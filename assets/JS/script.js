b5fd5d1ced59d5d8263eee672763dd5be11bbeb5
Alfredo <alfredofontinele08@gmail.com>
2022-05-15 11:51:57 -0300

    update 7

---
+++ assets/JS/script.js
@@ -0,0 +1,9 @@
+// window.onload = function() {
+//     document.querySelector(".menu_mobile").addEventListener("click", function() {
+//         if (document.querySelector(".header--right nav ul").style.display == "flex") {
+//             document.querySelector(".header--right nav ul").style.display = "none";
+//         } else {
+//             document.querySelector(".header--right nav ul").style.display = "flex";
+//         }
+//     })
+// };
