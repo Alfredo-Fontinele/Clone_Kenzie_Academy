d5c9405c5dbf13377ff3fd851c7ba4f89a4a3733
Alfredo <alfredofontinele08@gmail.com>
2022-05-15 12:29:35 -0300

    update 7

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,9 +1,21 @@
-// window.onload = function() {
-//     document.querySelector(".menu_mobile").addEventListener("click", function() {
-//         if (document.querySelector(".header--right nav ul").style.display == "flex") {
-//             document.querySelector(".header--right nav ul").style.display = "none";
-//         } else {
-//             document.querySelector(".header--right nav ul").style.display = "flex";
-//         }
-//     })
-// };
+// function pegarCardsInformations() {
+//     const cards = [
+//         "HTML5 E CSS3", 
+//         "Web Semântica", 
+//         "JavaScriptES6", 
+//         "Lógica de Programção", 
+//         "Controle de Versão com Git"
+//     ]
+    
+//     const header = document.createElement("h4");
+//     const cardInformation = document.getElementById("modulo1");
+
+//     header.innerText = "HARD SKILLS"
+// }
+
+// function adicionarClick() {
+//     const botao = document.getElementById("modulo1");
+//     botao.addEventListener("click", pegarCardsInformations);
+// }
+
+// adicionarClick()
